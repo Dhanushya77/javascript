51ddb0e41f82c3af2beb62cd1e91d4dc142ee57f
Dhanushya V S <75406996+Dhanushya77@users.noreply.github.com>
2024-12-12 16:23:34 +0530

    api using js

--- api_js/api_js.js
+++ api_js/api_js.js
@@ -3,18 +3,35 @@ fetch('https://dummyjson.com/products').then(response => response.json()).then(d
         const row = document.querySelector(".row")
         const col = document.createElement("div")
         col.classList.add('col-lg-3')
-        col.innerHTML = `
+        col.innerHTML =
+            `
         <div class="card" style="width:100%">
         <img class="card-img-top" src="${element.thumbnail}" alt="Card image" style="width:100%">
         <div class="card-body">
         <h4 class="card-title">${element.title}</h4>
         <p class="card-text">${element.description}</p>
-        <a href="#" class="btn btn-primary">See Profile</a>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" data-id="${element.id}">
+        View Product
+        </button>
         </div>
         </div>
         <br>
-        
         `
         row.appendChild(col)
     });
+
+    document.querySelectorAll(".btn").forEach(btn=>{
+        btn.addEventListener('click',(e)=>{
+            const pro_id=e.target.getAttribute('data-id')
+            const product=data.products.find((pro)=>pro.id==pro_id)
+            console.log(product);
+            document.getElementById("model_img").src=product.thumbnail
+            document.getElementById("title").innerHTML=product.title
+            document.getElementById("dis").innerHTML=`
+            ${product.description}<br>${product.price}$<br>${product.discountPercentage}%<br>
+            stock:${product.stock}
+            `
+        })
+    })
+
 })
